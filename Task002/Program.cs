2bff762f7121053eabb781aab24ad2237aca6f45
MaxX <maxx-f@mail.ru>
2022-09-01 21:06:58 +0300

    Add Task002

---
+++ Task002/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int m = SumNumber(n);
+
+int SumNumber(int n) 
+{
+    if (n > 0) return n % 10 + SumNumber(n / 10);
+    else return 0;
+}
+Console.WriteLine($"Сумма чисел, числа {n} - равна {m}.");
