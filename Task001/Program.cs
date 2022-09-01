a2985df25526688a862d2c981e9c64bee2552c8e
MaxX <maxx-f@mail.ru>
2022-09-01 20:40:48 +0300

    Add Task001

---
+++ Task001/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+
+Console.Write("Введите число N: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void PrintNumbers(int start, int end)
+{
+    if (start > end)
+    return;
+
+    Console.Write($"{start} ");
+    PrintNumbers(start + 1, end);
+}
+
+PrintNumbers(1, number);
