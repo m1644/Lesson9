a24e14cc711f563a7276ba4f6f9d52c6f631e6d5
MaxX <maxx-f@mail.ru>
2022-09-01 21:15:14 +0300

    Add Task003

---
+++ Task003/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии.
+
+Console.Write("Введите число A: ");
+double a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+double b = Convert.ToInt32(Console.ReadLine());
+
+double result = Degree(a, b); 
+
+double Degree(double a, double b)
+{
+    if (b > 1) return a * Degree(a, b-1);
+    return a;
+}
+Console.WriteLine($"Число {a} в степени {b} - равно {result}.");
